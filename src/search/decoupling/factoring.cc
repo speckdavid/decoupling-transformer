da6fbdf580df9b3e30ff77e3b1a29163ebfa1e2e
Daniel Gnad <daniel.gnad@liu.se>
2023-11-07 10:14:13 +0100

    get factoring to work

--- src/search/decoupling/factoring.cc
+++ src/search/decoupling/factoring.cc
@@ -1,17 +1,98 @@
 #include "factoring.h"
-#include "../utils/logging.h"
+
+#include "../plugins/plugin.h"
+
+#include "../tasks/root_task.h"
 
 using namespace std;
 
-namespace extra_tasks {
-Factoring::Factoring(const std::shared_ptr<AbstractTask> &task) : task(task) {
-    utils::g_log << "WARNING: THIS DECOUPLING ONLY WORKS FOR A SPECIALIZED TRANSPORT DOMAIN" << endl;
-    create_factoring();
+namespace decoupling {
+vector<Factoring::ActionSchema> Factoring::action_schemas;
+
+vector<set<int>> Factoring::var_to_affecting_op;
+
+Factoring::Factoring(const plugins::Options &opts) :
+    factoring_timer(utils::CountdownTimer(opts.get<int>("factoring_time_limit"))),
+    task(tasks::g_root_task),
+    task_proxy(TaskProxy(*task)),
+    min_number_leaves(opts.get<int>("min_number_leaves")),
+    max_leaf_size(opts.get<int>("max_leaf_size")) {
 }
 
-void Factoring::create_factoring() {
-    for (int var = 0; var < task->get_num_variables(); ++var) {
-        utils::g_log << task->get_variable_name(var) << endl;
+void Factoring::apply_factoring() {
+    // TODO implement
+}
+
+bool Factoring::check_timeout() const {
+    if (factoring_timer.is_expired()){
+        return false;
+    }
+    return true;
+}
+
+void Factoring::compute_action_schemas() {
+    if (action_schemas.empty()){
+        OperatorsProxy operators = TaskProxy(*task).get_operators();
+        assert(!operators.empty());
+        utils::HashMap<std::vector<int>, utils::HashMap<std::vector<int>, size_t> > scheme_loockup;
+        for (OperatorProxy op : operators) {
+            vector<int> pre_vars;
+            for (FactProxy pre : op.get_preconditions()) {
+                pre_vars.push_back(pre.get_variable().get_id());
+            }
+            sort(pre_vars.begin(), pre_vars.end());
+
+            vector<int> eff_vars;
+            for (EffectProxy eff : op.get_effects()) {
+                eff_vars.push_back(eff.get_fact().get_variable().get_id());
+            }
+            sort(eff_vars.begin(), eff_vars.end());
+
+            if (scheme_loockup.find(pre_vars) == scheme_loockup.end()){
+                scheme_loockup[pre_vars][eff_vars] = action_schemas.size();
+                action_schemas.emplace_back(1, pre_vars, eff_vars);
+            } else if (scheme_loockup[pre_vars].find(eff_vars) == scheme_loockup[pre_vars].end()){
+                scheme_loockup[pre_vars][eff_vars] = action_schemas.size();
+                action_schemas.emplace_back(1, pre_vars, eff_vars);
+            } else {
+                action_schemas[scheme_loockup[pre_vars][eff_vars]].incr_num_action();
+            }
+        }
     }
 }
+
+void Factoring::compute_var_to_ops_map() {
+    if (var_to_affecting_op.empty()) {
+        TaskProxy task_proxy(*task);
+        OperatorsProxy operators = task_proxy.get_operators();
+        var_to_affecting_op = vector<set<int> >(task_proxy.get_variables().size(), set<int>());
+        for (OperatorProxy op : operators) {
+            for (EffectProxy eff : op.get_effects()) {
+                var_to_affecting_op[eff.get_fact().get_variable().get_id()].insert(op.get_id());
+            }
+        }
+    }
+}
+
+void Factoring::add_options_to_feature(plugins::Feature &feature) {
+    feature.add_option<int>("min_number_leaves",
+                             "maximum number of leaves",
+                             "2");
+    feature.add_option<int>("max_leaf_size",
+                             "maximum domain size product of variables in a leaf",
+                             to_string(numeric_limits<int>::max() - 1));
+    feature.add_option<int>("factoring_time_limit",
+                               "timeout for computing the factoring",
+                               "infinity"
+    );
+}
+
+static class EvaluatorCategoryPlugin : public plugins::TypedCategoryPlugin<Factoring> {
+public:
+    EvaluatorCategoryPlugin() : TypedCategoryPlugin("Factoring") {
+        document_synopsis(
+                "A factoring that can be used for decoupled search.");
+    }
+}
+_category_plugin;
 }
--- src/search/decoupling/factoring.h
+++ src/search/decoupling/factoring.h
@@ -1,23 +1,60 @@
-#ifndef TASKS_FACTORING
+#ifndef DECOUPLING_FACTORING
+#define DECOUPLING_FACTORING
 
 #include "../abstract_task.h"
+#include "../task_proxy.h"
+
+#include "../plugins/options.h"
+#include "../utils/countdown_timer.h"
 
 #include <memory>
+#include <set>
 
-namespace extra_tasks {
-/*
-  Dummy Factoring Class to prototype decoupled search
-*/
+namespace decoupling {
 
 class Factoring {
+    utils::CountdownTimer factoring_timer;
+
+protected:
+
     std::shared_ptr<AbstractTask> task;
-    std::vector<std::vector<int>> leafs;
+    TaskProxy task_proxy;
 
-    void create_factoring();
+    std::vector<int> center;
+    std::vector<std::vector<int>> leaves;
+
+    struct ActionSchema {
+        int num_actions; // number of actions with the action schema
+        std::vector<int> pre_vars; // sorted
+        std::vector<int> eff_vars; // sorted
+
+        ActionSchema(int num_actions, const std::vector<int> &pre_vars, const std::vector<int> &eff_vars)
+                : num_actions(num_actions), pre_vars(pre_vars), eff_vars(eff_vars) {
+        }
+
+        void incr_num_action() {
+            num_actions++;
+        }
+    };
+
+    static std::vector<ActionSchema> action_schemas;
+
+    static std::vector<std::set<int> > var_to_affecting_op;
+
+    int min_number_leaves;
+    int max_leaf_size;
+
+    void compute_action_schemas();
+    void compute_var_to_ops_map();
+    bool check_timeout() const;
+
+    explicit Factoring(const plugins::Options &opts);
+    void apply_factoring();
+
 public:
-    Factoring(const std::shared_ptr<AbstractTask> &task);
+    virtual ~Factoring() = default;
 
-    int get_leaf_id(int var_id) const;
+    static void add_options_to_feature(plugins::Feature &feature);
 };
 }
 
