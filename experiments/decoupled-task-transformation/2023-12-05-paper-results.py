a5424dfae81d8d1c842b11e600d720d7cc11d026
Daniel Gnad <daniel.gnad@liu.se>
2023-12-06 21:19:31 +0100

    experiments

--- experiments/decoupled-task-transformation/2023-12-05-paper-results.py
+++ experiments/decoupled-task-transformation/2023-12-05-paper-results.py
@@ -42,8 +42,11 @@ exp.add_fetcher("/proj/parground/users/x_dangn/decoupled-fd/experiments/decoupli
 MS_REVISION = "8c2bbe375222e0ba6cdb83b4c79ee4ade6e884ad"
 exp.add_fetcher("/proj/parground/users/x_dangn/torralba-sievers-ijcai2019-fast-downward/experiments/decoupling-transformer/data/2023-12-04-ff-po-als-eval", name="fetch-ms", merge=True, filter=[remove_revision])
 
-ONE_L_REVISION = "32ef3a7297057a014a389c1291c3696f00d22ad4"
-exp.add_fetcher("data/2023-12-05-one-leaf-no-restriction-eval", name="fetch-one-leaf-no-res", merge=True, filter=[remove_revision])
+#ONE_L_REVISION = "32ef3a7297057a014a389c1291c3696f00d22ad4"
+#exp.add_fetcher("data/2023-12-05-one-leaf-no-restriction-eval", name="fetch-one-leaf-no-res", merge=True, filter=[remove_revision])
+
+NOOPT_ONE_L_REVISION = "32ef3a7297057a014a389c1291c3696f00d22ad4"
+exp.add_fetcher("data/2023-12-06-ff-no-concl-opt-eval", name="fetch-no-concl-opt-single-leaf-F", merge=True, filter=[remove_revision])
 
 
 attributes = common_setup.ATTRIBUTES
@@ -52,32 +55,32 @@ dec_filter = common_setup.NonDecoupledTaskFilter()
 
 exp.add_report(AbsoluteReport(attributes=attributes, filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs]), outfile=f"{SCRIPT_NAME}.html")
 
-factoring_references = [f"ff-{name}" for name in [#"L1.0s1M", "L1.0s1M1L", 
-                                                  "C1.0s1M", "C1.0s1M1L", #"C0.2s1M", "C0.2s1M1L", 
-                                                  "F0.2s1M", "F0.2s1M1L"]]
-
-for ref in factoring_references:
-    dec_filter = common_setup.NonDecoupledTaskFilter([ref])
-    exp.add_report(AbsoluteReport(attributes=attributes, filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs]), outfile=f"{SCRIPT_NAME}-filter-{ref[3:]}.html")
-
-def filter_only_lama(run):
-    if "lama" in run["algorithm"]:
-        return run
-    return False
-
-def filter_no_lama(run):
-    if "lama" not in run["algorithm"]:
-        return run
-    return False
-
-def filter_no_C_factoring(run):
-    if "C1.0" in run["algorithm"] or "c1" in run["algorithm"]:
-        return False
-    return run
-
-dec_filter = common_setup.NonDecoupledTaskFilter([f"ff-F0.2s1M"])
-exp.add_report(AbsoluteReport(attributes=["coverage"], filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, filter_no_C_factoring, filter_only_lama]), outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage-lama.html")
-exp.add_report(AbsoluteReport(attributes=["coverage"], filter=[dec_filter.filter_non_decoupled_runs, filter_no_C_factoring, filter_no_lama]), outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage-ff-po.html")
+#factoring_references = [f"ff-{name}" for name in [#"L1.0s1M", "L1.0s1M1L", 
+#                                                  "C1.0s1M", "C1.0s1M1L", #"C0.2s1M", "C0.2s1M1L", 
+#                                                  "F0.2s1M", "F0.2s1M1L"]]
+#
+#for ref in factoring_references:
+#    dec_filter = common_setup.NonDecoupledTaskFilter([ref])
+#    exp.add_report(AbsoluteReport(attributes=attributes, filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs]), outfile=f"{SCRIPT_NAME}-filter-{ref[3:]}.html")
+#
+#def filter_only_lama(run):
+#    if "lama" in run["algorithm"]:
+#        return run
+#    return False
+#
+#def filter_no_lama(run):
+#    if "lama" not in run["algorithm"]:
+#        return run
+#    return False
+#
+#def filter_no_C_factoring(run):
+#    if "C1.0" in run["algorithm"] or "c1" in run["algorithm"]:
+#        return False
+#    return run
+#
+#dec_filter = common_setup.NonDecoupledTaskFilter([f"ff-F0.2s1M"])
+#exp.add_report(AbsoluteReport(attributes=["coverage"], filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, filter_no_C_factoring, filter_only_lama]), outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage-lama.html")
+#exp.add_report(AbsoluteReport(attributes=["coverage"], filter=[dec_filter.filter_non_decoupled_runs, filter_no_C_factoring, filter_no_lama]), outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage-ff-po.html")
 
 class CompactCoverageFilter():
     def __init__(self):
@@ -89,48 +92,76 @@ class CompactCoverageFilter():
     def set_compact_coverage(self, run):
         run["compact_coverage"] = run["coverage"]
         if all(self.coverage[list(self.coverage.keys())[0]][run["domain"]] == self.coverage[alg][run["domain"]] for alg in self.coverage.keys()):
+            run["problem"] = run["domain"] + run["problem"]
             run["domain"] = "zzOther"
         return run
 
+BIG_TABLE_ALGORITHMS = ["ff-F0.2s1Mnopt", "ff-F0.2s1M", "ff-po", "DS-ff-F0.2s1M", "RS-a-ls", "dec-f02s1m-lama-first", "lama-first"]
+
 def remove_non_final_algorithms(run):
-    final_algorithms = ["ff-F0.2s1M", "ff-po", "DS-ff-F0.2s1M", "RS-a-ls", "dec-f02s1m-lama-first", "lama-first"]
-    if run["algorithm"] in final_algorithms:
+    if run["algorithm"] in BIG_TABLE_ALGORITHMS:
         return run
     return False
 
 FORMAT = "tex"
 
+dec_filter = common_setup.NonDecoupledTaskFilter(["ff-F0.2s1M"])
 comp_coverage = CompactCoverageFilter()
-exp.add_report(AbsoluteReport(attributes=[Attribute("compact_coverage", min_wins=False)], filter=[remove_non_final_algorithms, dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, comp_coverage.add_run, comp_coverage.set_compact_coverage], 
-                                                               filter_algorithm=["ff-F0.2s1M",
-                                                                                 "ff-po",
-                                                                                 "DS-ff-F0.2s1M",
-                                                                                 #"ff-MF",
-                                                                                 "RS-a-ls",
-                                                                                 "dec-f02s1m-lama-first",
-                                                                                 "lama-first",
-                                                                                 #"dec-mf-lama-first"
-                                                                                 ], 
-                                                                format=FORMAT,),
-               outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage.{FORMAT}")
+exp.add_report(AbsoluteReport(attributes=[Attribute("compact_coverage", absolute=True, min_wins=False)], filter=[remove_non_final_algorithms, dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, comp_coverage.add_run, comp_coverage.set_compact_coverage], filter_algorithm=BIG_TABLE_ALGORITHMS, format=FORMAT), outfile=f"{SCRIPT_NAME}-filter-F0.2s1M-coverage.{FORMAT}")
+
+MF_TABLE_ALGORITHMS = ["ff-Fs1L", "ff-po", "ff-MF", "dec-cs1l-lama-first", "lama-first", "dec-mf-lama-first"]
 
 def remove_non_1leaf_algorithms(run):
-    one_leaf_algorithms = ["ff-Cs1L", "ff-po", "ff-MF", "dec-cs1l-lama-first", "lama-first", "dec-mf-lama-first"]
-    if run["algorithm"] in one_leaf_algorithms:
+    if run["algorithm"] in MF_TABLE_ALGORITHMS:
         return run
     return False
 
 dec_filter = common_setup.NonDecoupledTaskFilter(["ff-MF"])
 comp_coverage = CompactCoverageFilter()
-exp.add_report(AbsoluteReport(attributes=[Attribute("compact_coverage", min_wins=False)], filter=[remove_non_1leaf_algorithms, dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, comp_coverage.add_run, comp_coverage.set_compact_coverage], 
-                              filter_algorithm=["ff-Cs1L",
-                                                "ff-po",
-                                                "ff-MF",
-                                                "dec-cs1l-lama-first",
-                                                "lama-first",
-                                                "dec-mf-lama-first"], 
-                              format="tex",),
-               outfile=f"{SCRIPT_NAME}-filter-mf-coverage-1L.{FORMAT}")
+exp.add_report(AbsoluteReport(attributes=[Attribute("compact_coverage", absolute=True, min_wins=False)], filter=[remove_non_1leaf_algorithms, dec_filter.add_runs, dec_filter.filter_non_decoupled_runs, comp_coverage.add_run, comp_coverage.set_compact_coverage], filter_algorithm=MF_TABLE_ALGORITHMS, format=FORMAT), outfile=f"{SCRIPT_NAME}-filter-mf-coverage-1L.{FORMAT}")
+
+
+
+
+class PlotTaskSizeSetter:
+    def __init__(self):
+        self.original_sizes = defaultdict(int)
+    
+    def set_plot_task_size(self, run):
+        size = None
+        inst = f"{run['domain']}-{run['problem']}"
+        if run["algorithm"] == "ff-po" and inst in self.original_sizes:
+            size = self.original_sizes[inst]
+        elif run["algorithm"] == "ff-F0.2s1M" and "task_size" in run:
+            size = run["task_size"]
+        run["plot_task_size"] = size
+        return run
+
+    def get_plot_task_size(self, run):
+        if run["algorithm"] == "ff-F0.2s1M" and "original_task_size" in run:
+            self.original_sizes[f"{run['domain']}-{run['problem']}"] = run["original_task_size"]
+        return run
+
+size_setter = PlotTaskSizeSetter()
+exp.add_report(
+    ScatterPlotReport(
+        attributes=["plot_task_size"],
+        filter=[size_setter.get_plot_task_size, size_setter.set_plot_task_size],
+        filter_algorithm=["ff-po", "ff-F0.2s1M"],
+        #get_category=domain_as_category,
+        format="png",  # Use "tex" for pgfplots output.
+    ),
+    name="scatterplot-task-size-transformation",
+)
+exp.add_report(
+    ScatterPlotReport(
+        attributes=["task_size"],
+        filter_algorithm=["ff-F0.2s1Mnopt", "ff-F0.2s1M"],
+        #get_category=domain_as_category,
+        format="png",  # Use "tex" for pgfplots output.
+    ),
+    name="scatterplot-task-size-optimization",
+)
 
 
 exp.run_steps()
---
+++ experiments/decoupled-task-transformation/2023-12-06-L-one-leaf-no-restriction.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python3
+
+import itertools
+import math
+import os
+from pathlib import Path
+import subprocess
+
+from lab.environments import TetralithEnvironment
+from lab.reports import Attribute, geometric_mean, arithmetic_mean
+
+from downward.reports.absolute import AbsoluteReport
+from downward.reports.compare import ComparativeReport
+from downward.reports.scatter import ScatterPlotReport
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+import decoupling_parser
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+REVISION = ""
+REVISIONS = [REVISION]
+
+CONFIGS = []
+factorings = {
+    'Ls1L' : 'lp(factoring_time_limit=30, strategy=mml, add_cg_sccs=true, min_number_leaves=1)',
+}
+heuristics = {"ff" : [["--evaluator", "hff=ff(transform=adapt_costs(one))"], "[hff], preferred=[hff]"],
+              #"gc" : [[], "[goalcount(cache_estimates=false)]"]
+}
+
+DRIVER_OPTS = ["--overall-memory-limit", "8G", "--overall-time-limit", "30m"]
+
+for h_name, heuristic_options in heuristics.items():
+    h_predefine, search_option = heuristic_options
+    for dec_name, dec in factorings.items():
+        CONFIGS.append(IssueConfig(f'{h_name}-{dec_name}', h_predefine + ['--root-task-transform', f"decoupled(factoring={dec})", '--search',  f'lazy_greedy({search_option})'], driver_options=DRIVER_OPTS))
+
+CONFIGS.append(IssueConfig("dec-ls1l-lama-first", [], driver_options=DRIVER_OPTS + ["--alias", "dec-ls1l-lama-first"]))
+
+SUITE = common_setup.DEFAULT_SATISFICING_SUITE
+
+ENVIRONMENT = TetralithEnvironment(
+    email="daniel.gnad@liu.se",
+#    time_limit_per_task="24:00:00",
+    memory_per_cpu="8300M",
+    extra_options="#SBATCH -A naiss2023-5-236", # parground
+)
+
+exp = IssueExperiment(
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+)
+
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+exp.add_parser(decoupling_parser.DecouplingParser())
+
+exp.add_step('build', exp.build)
+exp.add_step('start', exp.start_runs)
+exp.add_step("parse", exp.parse)
+exp.add_fetcher(name='fetch')
+
+BASE_REVISION = REVISION 
+DEC_REVISION = "613322a5185174645e76b9b63e00673e42e8e95d"
+exp.add_fetcher("data/2023-11-30-baselines-5m-eval", name="fetch-expl-base", filter_algorithm=[f"{BASE_REVISION}-ff-po", f"{BASE_REVISION}-gc"], merge=True)
+exp.add_fetcher("/proj/parground/users/x_dangn/decoupled-fd/experiments/decoupling-transformer/data/2023-11-22-baselines-5m-eval", name="fetch-dec-base", filter_algorithm=[f"{DEC_REVISION}-ff-L1.0s1M", f"{DEC_REVISION}-ff-F0.2s1M"], merge=True)
+exp.add_fetcher("/proj/parground/users/x_dangn/decoupled-fd/experiments/decoupling-transformer/data/2023-11-22-gc-5m-eval", name="fetch-dec-gc", merge=True)
+
+attributes = common_setup.ATTRIBUTES
+attributes.extend(exp.DEFAULT_TABLE_ATTRIBUTES)
+
+dec_filter = common_setup.NonDecoupledTaskFilter()
+
+exp.add_report(AbsoluteReport(attributes=attributes, filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs]), outfile=f"{SCRIPT_NAME}.html")
+
+factoring_references = [f"{REVISION}-ff-{name}" for name in factorings.keys()]
+
+for ref in factoring_references:
+    dec_filter = common_setup.NonDecoupledTaskFilter([ref])
+
+    exp.add_report(AbsoluteReport(attributes=attributes, filter=[dec_filter.add_runs, dec_filter.filter_non_decoupled_runs]), outfile=f"{SCRIPT_NAME}-filter-{ref[41:]}.html")
+
+
+ 
+
+exp.run_steps()
+
+dec_filter.print_statistics()
+
