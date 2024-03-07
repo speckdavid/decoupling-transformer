39d4e95b249532f08ca31b5a98ac96c6ced9b46b
Daniel Gnad <daniel.gnad@liu.se>
2024-03-07 14:37:08 +0100

    remove experimental aliases and rename the final ones

--- driver/aliases.py
+++ driver/aliases.py
@@ -133,52 +133,7 @@ ALIASES["lama-first"] = [
     """lazy_greedy([hff,hlm],preferred=[hff,hlm],
                                cost_type=one,reopen_closed=false)))"""]
 
-ALIASES["dec-ls1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mml, add_cg_sccs=true, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-ls1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mml, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-l1s1m-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mml, min_flexibility=1.0, add_cg_sccs=true, max_leaf_size=1000000))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-l1s1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mml, min_flexibility=1.0, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-f02s-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mfa, min_flexibility=0.2, add_cg_sccs=true))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-f02s1m-lama-first"] = [
+ALIASES["decoupled-lama-first"] = [
     "--root-task-transform",
     "decoupled(factoring=lp(factoring_time_limit=30, strategy=mfa, min_flexibility=0.2, add_cg_sccs=true, max_leaf_size=1000000))",
     "--search",
@@ -187,70 +142,7 @@ ALIASES["dec-f02s1m-lama-first"] = [
     """lazy_greedy([hff,hlm],preferred=[hff,hlm],
                                cost_type=one,reopen_closed=false)))"""]
 
-ALIASES["dec-f02s1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mfa, min_flexibility=0.2, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-c1s1m-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, min_flexibility=1.0, add_cg_sccs=true, max_leaf_size=1000000))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-c1s1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, min_flexibility=1.0, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-cs1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, add_cg_sccs=true, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-cs1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-c02s1m-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, min_flexibility=0.2, add_cg_sccs=true, max_leaf_size=1000000))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-c02s1m1l-lama-first"] = [
-    "--root-task-transform",
-    "decoupled(factoring=lp(factoring_time_limit=30, strategy=mcl, min_flexibility=0.2, add_cg_sccs=true, max_leaf_size=1000000, min_number_leaves=1))",
-    "--search",
-    "let(hlm, landmark_sum(lm_factory=lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(one),pref=false),"
-    "let(hff, ff(transform=adapt_costs(one)),"
-    """lazy_greedy([hff,hlm],preferred=[hff,hlm],
-                               cost_type=one,reopen_closed=false)))"""]
-
-ALIASES["dec-mf-lama-first"] = [
+ALIASES["miura-lama-first"] = [
     "--root-task-transform",
     "decoupled(factoring=mf())",
     "--search",
