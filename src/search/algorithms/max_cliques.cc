93cdda7aff346ccaa984d889ea53e423fcb78511
speckdavid <davidjakob.speck@gmail.com>
2024-07-03 15:15:23 +0200

    added algorithm to compute max weighted clique and max weighted independent set as backbone for factoring

--- src/search/algorithms/max_cliques.cc
+++ src/search/algorithms/max_cliques.cc
@@ -7,11 +7,32 @@
 #include <cassert>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <vector>
 
 using namespace std;
 
 namespace max_cliques {
+static bool is_edge(const vector<int> &neighbors, int node) {
+    return find(neighbors.begin(), neighbors.end(), node) != neighbors.end();
+}
+
+static vector<vector<int>> create_complement(const vector<vector<int>> &graph) {
+    int n = graph.size();
+    vector<vector<int>> complement_graph(n);
+
+    // Create the complement graph
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j && !is_edge(graph[i], j)) {
+                complement_graph[i].push_back(j);
+            }
+        }
+    }
+
+    return complement_graph;
+}
+
 class MaxCliqueComputer {
     const vector<vector<int>> &graph;
     vector<vector<int>> &max_cliques;
@@ -114,5 +135,145 @@ void compute_max_cliques(
     vector<vector<int>> &max_cliques) {
     MaxCliqueComputer clique_computer(graph, max_cliques);
     clique_computer.compute();
+}
+
+void compute_max_independent_sets(
+    const vector<vector<int>> &graph,
+    vector<vector<int>> &max_independent_sets) {
+    auto complement_graph = create_complement(graph);
+    MaxCliqueComputer clique_computer(complement_graph, max_independent_sets);
+    clique_computer.compute();
+}
+
+class MaxWeightCliqueComputer {
+private:
+    const vector<vector<int>> &graph;
+    const vector<int> &node_weights;
+    vector<int> incumbent_nodes;
+    int incumbent_weight;
+
+    void update_incumbent_if_improved(const vector<int> &C, int C_weight) {
+        if (C_weight > incumbent_weight) {
+            incumbent_nodes = C;
+            incumbent_weight = C_weight;
+        }
+    }
+
+    vector<int> greedily_find_independent_set(vector<int> P) {
+        vector<int> independent_set;
+        while (!P.empty()) {
+            int v = P[0];
+            independent_set.push_back(v);
+            P.erase(P.begin());
+            P.erase(remove_if(P.begin(), P.end(), [this, v](int w) {
+                                  return find(graph.at(v).begin(), graph.at(v).end(), w) != graph.at(v).end();
+                              }), P.end());
+        }
+        return independent_set;
+    }
+
+    vector<int> find_branching_nodes(vector<int> P, int target) {
+        unordered_map<int, int> residual_wt;
+        for (int v : P) {
+            residual_wt[v] = node_weights[v];
+        }
+
+        int total_wt = 0;
+        while (!P.empty()) {
+            vector<int> independent_set = greedily_find_independent_set(P);
+            int min_element_in_class = *min_element(independent_set.begin(), independent_set.end(),
+                                                    [&residual_wt](int a, int b) {
+                                                        return residual_wt[a] < residual_wt[b];
+                                                    });
+            int min_wt_in_class = residual_wt[min_element_in_class];
+
+            total_wt += min_wt_in_class;
+            if (total_wt > target)
+                break;
+
+            for (int v : independent_set) {
+                residual_wt[v] -= min_wt_in_class;
+            }
+
+            P.erase(remove_if(P.begin(), P.end(), [&residual_wt](int v) {
+                                  return residual_wt[v] == 0;
+                              }), P.end());
+        }
+        return P;
+    }
+
+    void expand(vector<int> C, int C_weight, vector<int> P) {
+        update_incumbent_if_improved(C, C_weight);
+        vector<int> branching_nodes = find_branching_nodes(P, incumbent_weight - C_weight);
+
+        while (!branching_nodes.empty()) {
+            int v = branching_nodes.back();
+            branching_nodes.pop_back();
+            P.erase(remove(P.begin(), P.end(), v), P.end());
+
+            vector<int> new_C = C;
+            new_C.push_back(v);
+
+            int new_C_weight = C_weight + node_weights[v];
+            vector<int> new_P;
+            for (int w : P) {
+                if (find(graph.at(v).begin(), graph.at(v).end(), w) != graph.at(v).end()) {
+                    new_P.push_back(w);
+                }
+            }
+            expand(new_C, new_C_weight, new_P);
+        }
+    }
+
+public:
+    MaxWeightCliqueComputer(const vector<vector<int>> &graph, const vector<int> &weights)
+        : graph(graph), node_weights(weights), incumbent_weight(0) {
+        if (graph.size() != weights.size()) {
+            cerr << "Number of nodes does not match number of weights!" << endl;
+            utils::exit_with(utils::ExitCode::SEARCH_INPUT_ERROR);
+        }
+        if (graph.size() == 0) {
+            cerr << "Graph is empty!" << endl;
+            utils::exit_with(utils::ExitCode::SEARCH_INPUT_ERROR);
+        }
+    }
+
+    int find_max_weight_clique(vector<int> &max_clique) {
+        if (graph.size() == 0) {
+            return 0;
+        }
+
+        vector<int> nodes(graph.size());
+        iota(nodes.begin(), nodes.end(), 0);
+
+        sort(nodes.begin(), nodes.end(), [this](int a, int b) {
+                 return graph.at(a).size() > graph.at(b).size();
+             });
+
+        nodes.erase(remove_if(nodes.begin(), nodes.end(), [this](int v) {
+                                  return node_weights[v] <= 0;
+                              }), nodes.end());
+
+        expand({}, 0, nodes);
+        max_clique = incumbent_nodes;
+        return incumbent_weight;
+    }
+};
+
+int compute_max_weighted_clique(
+    const vector<vector<int>> &graph,
+    const vector<int> &weights,
+    vector<int> &max_clique) {
+    MaxWeightCliqueComputer computer(graph, weights);
+    return computer.find_max_weight_clique(max_clique);
+}
+
+extern int compute_max_weighted_independent_set(
+    const vector<vector<int>> &graph,
+    const vector<int> &weights,
+    vector<int> &independent_set) {
+    auto complement_graph = create_complement(graph);
+    MaxWeightCliqueComputer computer(complement_graph, weights);
+    return computer.find_max_weight_clique(independent_set);
 }
 }
--- src/search/algorithms/max_cliques.h
+++ src/search/algorithms/max_cliques.h
@@ -21,6 +21,39 @@ namespace max_cliques {
 extern void compute_max_cliques(
     const std::vector<std::vector<int>> &graph,
     std::vector<std::vector<int>> &max_cliques);
-}
 
+extern void compute_max_independent_sets(
+    const std::vector<std::vector<int>> &graph,
+    std::vector<std::vector<int>> &max_independent_sets);
+
+/* Implementation of the Weighted Max Clique algorithm implemented
+   in the python networkx package:
+
+   The implementation is recursive, and therefore it may run into recursion
+   depth issues if G contains a clique whose number of nodes is close to the
+   recursion depth limit.
+
+   At each search node, the algorithm greedily constructs a weighted independent
+   set cover of part of the graph in order to find a small set of nodes on which
+   to branch. The algorithm is very similar to the algorithm of Tavares et al. [1],
+   other than the fact that the NetworkX version does not use bitsets. This style of
+   algorithm for maximum weight clique (and maximum weight independent set, which is the
+   same problem but on the complement graph) has a decades-long history. See Algorithm B of
+   Warren and Hicks [2] and the references in that paper.
+
+   [1] Tavares, W.A., Neto, M.B.C., Rodrigues, C.D., Michelon, P.: Um algoritmo de branch and
+       bound para o problema da clique máxima ponderada. Proceedings of XLVII SBPO 1 (2015).
+   [2] Warren, Jeffrey S, Hicks, Illya V.: Combinatorial Branch-and-Bound for the Maximum Weight
+       Independent Set Problem. Technical Report, Texas A&M University (2016).
+ */
+extern int compute_max_weighted_clique(
+    const std::vector<std::vector<int>> &graph,
+    const std::vector<int> &weights,
+    std::vector<int> &max_clique);
+
+extern int compute_max_weighted_independent_set(
+    const std::vector<std::vector<int>> &graph,
+    const std::vector<int> &weights,
+    std::vector<int> &independent_set);
+}
 #endif
